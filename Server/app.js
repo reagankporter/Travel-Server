1c86d92cb308af8551e532f98fce3324f0addd73
reagankporter <reagankporter@gmail.com>
2021-10-27 15:56:08 -0400

    Login Server

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,18 @@
-const Express = require ('express');
-const app = Express ();
-
-app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000.`);
+const Express = require('express');
+const app = Express();
+const dbConnection = require('./db');
+const controllers = require("./controllers");
+     
+     app.use('/journal', controllers.journalController);
+     app.use('/user', controllers.userController);
+     
+    dbConnection.authenticate()
+      .then(() => dbConnection.sync())
+      .then(() => {
+        app.listen(3000, () => {
+          console.log(`[Server]: App is listening on 3000.`);
+        });
+      })
+    .catch((err) => {
+    console.log(`[Server]: Server crashed. Error = ${err}`);
 });
