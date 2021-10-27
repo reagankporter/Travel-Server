4bb92958fe8642c921b752dc39d0aebea08dc862
Ginea Merrill <gineakanoe@gmail.com>
2021-10-27 15:55:07 -0400

    added info to app js page

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,26 @@
+require('dotenv').config();
 const Express = require ('express');
-const app = Express ();
+const app = Express();
+const dbConnection = require('./db');
 
-app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000.`);
-});
+const controllers = require('./Controllers');
+
+app.use(Express.json());
+
+app.use('/user', controllers.userController);
+
+app.use(require('./middleware/validate-jwt'));
+
+app.use('journal', controllers.journalController);
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`[Server]: App is listening on 3000.`);
+        });
+    })
+    .catch((err) => {
+        console.log(`[Server]: Server crashed.  Error = ${err}`);
+    });
+
