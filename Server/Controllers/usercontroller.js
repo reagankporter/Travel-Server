619e7ffbd5869693294df32ef845a06da604498b
Ginea Merrill <gineakanoe@gmail.com>
2021-10-28 12:14:56 -0400

    local changes

--- Server/Controllers/usercontroller.js
+++ Server/Controllers/usercontroller.js
@@ -1,13 +1,26 @@
 const router = require('express').Router();
 const { UserModel } = require('../Models'); 
 
+// router.post('/register', async (req, res) => {    
+//     UserModel.create({
+//         username: 'billybob',
+//         email: 'billy@email.com',
+//         password: 'password1234'
+//     })
+// })
+
 router.post('/register', async (req, res) => {
+
+
+        let {username, email, password} = req.body.user;
          
-        UserModel.create({
-            username: 'testname456',
-            email: 'user2@email.com',
-            password: 'password1234'
-        })
-    })
+        await UserModel.create({
+
+            username,
+            email,
+            password,
+        });
+        res.send('This is out user/register endpoint!');
+});
 
 module.exports = router;
