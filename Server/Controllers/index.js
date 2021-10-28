2db58b9aed8d175913831bf1e1a20a545952506a
reagankporter <reagankporter@gmail.com>
2021-10-28 09:10:06 -0400

    App is Listening Commit

--- Server/Controllers/index.js
+++ Server/Controllers/index.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 module.exports = {
     userController: require('./userController'),
-    journalController: require('./journalController'),
+    journalController: require('./journalcontroller'),
  }
-=======
-module.exports ={
-    journalController: require("./journalcontroller"),
-};
->>>>>>> d56ed01b0f3ae4ab05effc1c5aeaff7e2e5e82ce
+
+
--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Express = require('express');
 const app = Express();
 const dbConnection = require('./db');
@@ -17,29 +17,10 @@ const controllers = require("./Controllers");
     .catch((err) => {
     console.log(`[Server]: Server crashed. Error = ${err}`);
 });
-=======
+
 require('dotenv').config();
-const Express = require ('express');
-const app = Express();
-const dbConnection = require('./db');
-const controllers = require('./Controllers');
-
 app.use(Express.json());
-
-app.use('/user', controllers.userController);
-
 app.use(require('./middleware/validate-jwt'));
 
-app.use ('/journal',controllers.journalController);
 
-dbConnection.authenticate()
-    .then(() => dbConnection.sync())
-    .then(() => {
-        app.listen(3000, () => {
-            console.log(`[Server]: App is listening on 3000.`);
-        });
-    })
-    .catch((err) => {
-        console.log(`[Server]: Server crashed.  Error = ${err}`);
-    });
->>>>>>> d56ed01b0f3ae4ab05effc1c5aeaff7e2e5e82ce
+
