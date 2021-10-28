9f305950e884f5716d17a0d5252f01ea5441e9e9
reagankporter <reagankporter@gmail.com>
2021-10-28 12:03:20 -0400

    edited login endpoint

--- Server/Controllers/usercontroller.js
+++ Server/Controllers/usercontroller.js
@@ -3,33 +3,42 @@ const { UserModel } = require('../Models'); 
 const User = require('../Models/user');
 const { UniqueConstraintError } = require("sequelize/lib/errors");
 
-router.post('/register', async (req, res) => {
-
-        let {username, email, password} = req.body.user;
-        try{
-        await UserModel.create({
+router.post("/register", async (req, res) => {
+          let { username, email, password } = req.body.user;
+          try {
+           const User = await UserModel.create({
             username,
-            email,
-            password,
-        });
+            email,
+            password,
+          });
+        
+          res.status(201).json({
+           message: "User successfully registered",
+           user: User,
+          });
+         } catch (err) {
+              if (err instanceof UniqueConstraintError) {
+                   res.status(409).json({
+                    message: "Email already in use",
+                   });
+                  } else {
+                   res.status(500).json({
+                    message: "Failed to register user",
+                   });
+              }
+         }
+        });
 
-    res.status(201).json({
-        message: 'User Succesfully Registered! Thank you!',
-        user: User
-    });
-    } catch (err) {
 
-    if (err instanceof UniqueConstraintError) {
-        res.status(409).json({
-        message: "Email already in use",
-    });
-    } else {
-        res.status(500).json({
-        message: "Failed to register user! Try Again!",
-        });
-    }
-}
-});
+
+router.post("/login", async (req, res) => {
+      let {username, email, password } = req.body.user;    
+        await UserModel.findOne({
+        where: {
+              email: email,
+        },
+      });
+    });
 
 
 module.exports = router;
