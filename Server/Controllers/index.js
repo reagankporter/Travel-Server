61095432000e7f8fe4050d6f72068b96c5326d4d
Ginea Merrill <gineakanoe@gmail.com>
2021-10-28 12:17:35 -0400

    fix folder change issue hopefully

---
+++ Server/Controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    userController: require('./userController'),
+    journalController: require('./journalController'),
+    bucketListController: require('./bucketListController'),
+ };
+
