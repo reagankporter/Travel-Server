d986cc33d92606f487cf60568aed325d5f8364c1
Ginea Merrill <gineakanoe@gmail.com>
2021-11-03 10:01:58 -0400

    saving server changes

--- Server/db.js
+++ Server/db.js
@@ -4,6 +4,8 @@ const {Sequelize} = require('sequelize');
 
 // const sequelize = new Sequelize("postgres://postgres:maple2323@localhost:5432/traveljournal");
 
+// const sequelize = new Sequelize('postgres://postgres:14b9c4a2e2ce45508f4991456a37ae47@localhost:5432/travel-server');  // Ginea's Postgres
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
