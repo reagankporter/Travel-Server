feb1bb50a33ff6ba963663b03af8b09eee25ab5c
Ginea Merrill <gineakanoe@gmail.com>
2021-11-03 20:41:44 -0400

    pulled from develop and fixed it to my postgres

--- Server/db.js
+++ Server/db.js
@@ -2,11 +2,6 @@ const {Sequelize} = require('sequelize');
 
 // const sequelize = new Sequelize("postgres://postgres:6655a365d362406995af1a41383ebb21@localhost:5432/Travel-Server");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:"postgres",
-    ssl: process.env.ENVIROMENT === "production"
-});
-
 // const sequelize = new Sequelize('postgres://postgres:14b9c4a2e2ce45508f4991456a37ae47@localhost:5432/travel-server');  // Ginea's Postgres
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
